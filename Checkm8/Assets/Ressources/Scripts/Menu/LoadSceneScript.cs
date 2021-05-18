b62c25549c11e5791cfd711776043643ae13f2c7
Goubeau Samuel <sams86240@gmail.com>
2021-05-18 10:52:54 +0200

    Add new method, to load the previous scene

--- Checkm8/Assets/Ressources/Scripts/Menu/LoadSceneScript.cs
+++ Checkm8/Assets/Ressources/Scripts/Menu/LoadSceneScript.cs
@@ -5,19 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class LoadSceneScript : MonoBehaviour
 {
+    private static int previousScene;
 
     /**
-    * Exit game when the program call it
-    */
+     * Exit game when the program call it
+     */
     public void quitGame(){
         Debug.Log("exitgame");
         Application.Quit();
     }
 
     /**
-    * Load scene in parameter
-    */
+     * Load scene in parameter
+     */
     public void changeScene(string scene){
+        previousScene = SceneManager.GetActiveScene().buildIndex;
+
         SceneManager.LoadScene(scene);
+    }
+
+    /** 
+     * Load the previous scene
+     */
+    public void loadPreviousScene()
+    {
+        if (previousScene != null)
+        {
+            SceneManager.LoadScene(previousScene);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
