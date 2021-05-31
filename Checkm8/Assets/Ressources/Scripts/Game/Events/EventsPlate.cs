cdeb94c40abf35ff001d6f6e1806b631cb70e8b2
MaximeQuertan <74728205+MaximeQuertan@users.noreply.github.com>
2021-05-31 11:52:22 +0200

    debut event demo

---
+++ Checkm8/Assets/Ressources/Scripts/Game/Events/EventsPlate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsPlate : MonoBehaviour
+{
+    public int eventNum;
+   //Position du board
+    int matrixX;
+    int matrixY;
+
+    public void SetCoords(int posX, int posY)
+    {
+        matrixX = posX;
+        matrixY = posY;
+    }
+
+    public void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.gameObject.CompareTag("piece"))
+        {
+            switch(eventNum)
+            {
+                case 5 :
+                    EventsManager.Demotion();
+                    Destroy(gameObject);
+                    Debug.Log("demo");
+                    break;
+            }
+        }
+    }
+}
