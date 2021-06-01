ce38c079d784e2a0acd70334f7dab2a4e52a9795
Goubeau Samuel <sams86240@gmail.com>
2021-06-01 11:57:34 +0200

    Begin implement beginner player

---
+++ Checkm8/Assets/Ressources/Scripts/Menu/SelectionScene/BeginnerToken.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/**
+ * Author : Samuel Goubeau
+ * 
+ * Manage : 
+ * 
+ * Using : SelectionScene2P.Canvas.Canvas-P1, SelectionScene2P.Canvas.Canvas-P2,
+ *         SelectionScene4P.Canvas.Canvas-P1, SelectionScene4P.Canvas.Canvas-P2, 
+ *         SelectionScene4P.Canvas.Canvas-P3, SelectionScene4P.Canvas.Canvas-P4
+ */
+public class BeginnerToken : MonoBehaviour
+{
+    [SerializeField] private GameObject tkBeginner;
+
+    void Start()
+    {
+        tkBeginner.SetActive(false);
+    }
+}
