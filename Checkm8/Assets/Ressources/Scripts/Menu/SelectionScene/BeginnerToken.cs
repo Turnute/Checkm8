9cbdfcf05022b0efc2a631ce483bdbcbb5ad9669
Goubeau Samuel <sams86240@gmail.com>
2021-06-02 11:53:37 +0200

    Continue to implement beginner player

--- Checkm8/Assets/Ressources/Scripts/Menu/SelectionScene/BeginnerToken.cs
+++ Checkm8/Assets/Ressources/Scripts/Menu/SelectionScene/BeginnerToken.cs
@@ -13,12 +13,57 @@ using UnityEngine.UI;
  *         SelectionScene4P.Canvas.Canvas-P1, SelectionScene4P.Canvas.Canvas-P2, 
  *         SelectionScene4P.Canvas.Canvas-P3, SelectionScene4P.Canvas.Canvas-P4
  */
-public class BeginnerToken : MonoBehaviour
+public class BeginnerToken : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] private GameObject tkBeginner;
+    [SerializeField] private GameObject tkBeginnerCur;
+    [SerializeField] private GameObject tkBeginnerOther;
+
+    private bool isActive;
+    private bool isIn;
+    private CanvasGroup curCanvasGroup;
+    private CanvasGroup otherCanvasGroup;
+
 
     void Start()
     {
-        tkBeginner.SetActive(false);
+        curCanvasGroup = tkBeginnerCur.GetComponent<CanvasGroup>();
+        otherCanvasGroup = tkBeginnerOther.GetComponent<CanvasGroup>();
+
+        isActive = false;
+        curCanvasGroup.alpha = 0f;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("isIn");
+        isIn = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("isOut");
+        isIn = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Fire2") > 0.5f && isIn)
+        {
+            if (isActive)
+            {
+                isActive = false;
+                curCanvasGroup.alpha = 0f;
+            }
+            else
+            {
+                if(otherCanvasGroup.alpha != 0f)
+                {
+                    otherCanvasGroup.alpha = 0f;
+                }
+
+                isActive = true;
+                curCanvasGroup.alpha = 1f;
+            }
+        }
     }
 }
